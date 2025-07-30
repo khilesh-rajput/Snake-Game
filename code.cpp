1936d3dffb36fceb62c6e41cf59ca6794dd34130
Khilesh Rajput <khileshrajput2005@gmail.com>
2025-07-30 14:19:56 +0530

    setup file updated

--- code.cpp
+++ code.cpp
@@ -5,8 +5,23 @@ using namespace std;
 bool GameOver;
 const int height = 20;
 const int width = 20;
-void setup(){
+int x,y,fruitX,fruitY,score;
 
+/* 
+enum in C++ is a user-defined data type that assigns names to a set of constant integer values,
+making the code more readable and organized
+*/
+enum Direction {STOP = 0, LEFT, RIGHT, UP, DOWN};
+Direction dir;
+
+void setup(){
+    GameOver = false;
+    dir = STOP;
+    x = width/2;
+    y = height/2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+    score = 0;
 }
 
 void draw(){
@@ -23,6 +38,6 @@ void logic(){
 
 
 int main(){
-
+    setup();
     return 0;
 }
