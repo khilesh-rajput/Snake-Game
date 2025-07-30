42ab50a2f4a59c89ac4d5f3d11da841bfd96a18e
Khilesh Rajput <khileshrajput2005@gmail.com>
2025-07-30 13:52:57 +0530

    defined structure

--- code.cpp
+++ code.cpp
@@ -1,1 +1,28 @@
 // This is code for the snake game using c++ language 
+#include <iostream>
+using namespace std;
+
+bool GameOver;
+const int height = 20;
+const int width = 20;
+void setup(){
+
+}
+
+void draw(){
+
+}
+
+void input(){
+
+}
+
+void logic(){
+
+}
+
+
+int main(){
+
+    return 0;
+}
